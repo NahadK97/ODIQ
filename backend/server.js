c1084ca914a1add67596447877e7667cea8b2b09
Nahad Bin Noushad <nahadnowshad14@gmail.com>
2025-06-07 17:15:10 +0400

    Installing files

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+// express app
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// log requests
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.use("/api/workouts", workoutRoutes);
+
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests once connected to db
+    app.listen(process.env.PORT, () => {
+      console.log(
+        "Connected to DB and server is listening on port",
+        process.env.PORT
+      );
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
+  });
