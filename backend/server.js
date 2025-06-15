641353da1b6953c93d6cd01a940312aeea006341
Nahad Bin Noushad <nahadnowshad14@gmail.com>
2025-06-15 16:35:37 +0400

    Auth Backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const questionRoutes = require("./routes/questions");
+const userRoutes = require("./routes/user");
 // express app
 const app = express();
 
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/questions", questionRoutes);
+app.use("/api/user", userRoutes);
 
 // connect to db
 mongoose
